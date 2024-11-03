267128ec09f803b34de1d8da6723bf22024e1e09
macbook <macbook@MacBook-Air-ihajar.local>
2024-11-03 17:39:22 +0100

    42: Fix OAuth in prod

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -10,12 +10,12 @@ import { createAdminClient } from "@/lib/appwrite";
 export async function signUpWithGoogle() {
     const { account } = await createAdminClient();
 
-    // const origin = headers().get("origin");
+    const origin = headers().get("origin");
 
     const redirectUrl = await account.createOAuth2Token(
         OAuthProvider.Google,
-        `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-        `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+        `${origin}/oauth`,
+        `${origin}/sign-up`,
     );
 
     return redirect(redirectUrl);
